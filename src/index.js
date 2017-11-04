76e9c2ecc3c6acc3368f3a7d142bd30bb7f16c63
Andrew Newman <andrew.newman@sparesbox.com.au>
2017-11-05 02:49:02 +1100

    updated global stylesheet to styled component injectGlobal

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import 'core-js/es6/map';
 import 'core-js/es6/set';
-
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import hcardApp from './reducers';
 import App from './components/App/App';
-
-import './index.css';
+import './uiGlobal';
 
 let store = createStore(
   hcardApp,
