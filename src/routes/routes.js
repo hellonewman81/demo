69181661329dbeee649ae230b6164d3c97be3cb3
Andrew Newman <andrew.newman@sparesbox.com.au>
2017-11-06 09:10:17 +1100

    updated App module - format

---
+++ src/routes/routes.js
@@ -0,0 +1,26 @@
+import AppRoot from './app-root';
+import Home from './home';
+import List from './list';
+
+const routes = [
+  {
+    component: AppRoot,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        component: Home
+      },
+      {
+        path: '/home',
+        component: Home
+      },
+      {
+        path: '/list',
+        component: List
+      }
+    ]
+  }
+];
+
+export default routes;
